978972ab9f8638ede27f68db495c1caab255fa87
Victor King'e <vinge1718@gmail.com>
2017-09-17 11:35:28 +0300

    add the delete task to the gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,9 @@ var gulp = require("gulp");
 var browserify = require("browserify");
 var source = require("vinyl-source-stream");
 var uglify = require("gulp-uglify");
+var utilities = require("gulp-util");
+var buildProduction = utilities.env.production;
+var dell = require("del");
 /*
 var concat = require("gulp-concat")
 
@@ -11,6 +14,10 @@ gulp.task("concatenate", function(){
   .pipe(gulp.dest("./tmp"));
 });
 */
+
+gulp.task("clean", function(){
+  return del(["build"])
+});
 
 gulp.task("minifyScripts", ["jsBrowserify"], function(){
   return gulp.src("./build/js/apps.js")
@@ -24,3 +31,11 @@ gulp.task("jsBrowserify",function(){
   .pipe(source("app.js"))
   .pipe(gulp.dest("./build/js"));
 })
+
+gulp.task("build", function(){
+  if (buildProduction){
+    gulp.start("minifyScripts");
+  }else {
+    gulp.start("jsBrowserify");
+  }
+});
