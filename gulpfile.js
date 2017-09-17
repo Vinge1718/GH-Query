8f152702d915f0977ee97f2813f1f8c21198f1bd
Victor King'e <vinge1718@gmail.com>
2017-09-17 10:10:36 +0300

    add a gulp task to minify scripts for easier browser parsing

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,24 @@
 var gulp = require("gulp");
 var browserify = require("browserify");
 var source = require("vinyl-source-stream");
+var uglify = require("gulp-uglify");
+/*
+var concat = require("gulp-concat")
 
-gulp.task("jsBrowserify", function(){
+gulp.task("concatenate", function(){
+  return gulp.src(["./js/*-interface.js"])
+  .pipe(concat("allFrontEnd.js"))
+  .pipe(gulp.dest("./tmp"));
+});
+*/
+
+gulp.task("minifyScripts", ["jsBrowserify"], function(){
+  return gulp.src("./build/js/apps.js")
+  .pipe(uglify())
+  .pipe(gulp.dest("./build/js"));
+});
+
+gulp.task("jsBrowserify",function(){
   return browserify({ entries: ["./js/gh-query-interface.js"]})
   .bundle()
   .pipe(source("app.js"))
