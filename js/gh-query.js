fef04c6222adb4ec04dacd0690d64553f8142ef4
Victor King'e <vinge1718@gmail.com>
2017-09-18 13:22:07 +0300

    add the backend key within the constructor in order to include it in the call-back functtion  in another file (hg-query-innterface.js) without declaring it again

--- js/gh-query.js
+++ js/gh-query.js
@@ -1,12 +1,18 @@
-var apiKey = require("./../.env").apiKey;
-
 function Search(){
 
 }
 
 Search.prototype.query = function (userName) {
-  $.get("http://api.github.com/users/" + userName + "?access_token=" + apiKey).then(function(response){
-    console.log(response);
+  var apiKey = require("./../.env").apiKey;
+  $.get("https://api.github.com/users/" + userName + "?access_token=" + apiKey).then(function(response){
+    $("#avatar").append("<img src="+ response.avatar_url +" height=\"200\">");
+    $("#about").append("<p>Results: </p><br>"+response.login+"<ul><li>Number of Repositories: "+response.public_repos+
+    "</li><li>Users following this account: "+response.followers+"</li><li>Accounts followed by this User: "+response.following+"</li></ul>")
+    $.get("https://api.github.com/users/" +userName+"/repos?access_token="+apiKey).then(function(response){
+      for(var i = 0; i < response.length; i++){
+      $("#repos").append("<h4> Repository name: "+response[i].name+ "</h4>"+ "<ul><li>Dominant Languages Implemented: "+response[i].language+"</li><li>The project is located at: "+response[i].html_url+"</li></ul>");
+    }
+    });
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
